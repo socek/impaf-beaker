2a60410898a2c897e0dcbb5a40f48847b9ab8681
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2015-06-18 21:34:42 +0200

    Renamed feed_request to _unpack_request

--- src/implugin/beaker.py
+++ src/implugin/beaker.py
@@ -11,6 +11,6 @@ class BeakerApplication(Application):
 
 class BeakerRequestable(Requestable):
 
-    def feed_request(self, request):
-        super().feed_request(request)
+    def _unpack_request(self, request):
+        super()._unpack_request(request)
         self.session = request.session
